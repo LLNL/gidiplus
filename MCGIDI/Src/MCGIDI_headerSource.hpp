51ba8688f30a43d7928fde8c2aa723c47a0618c4
Caleb Mattoon <mattoon1@llnl.gov>
2025-03-11 11:16:18 -0700

    Define _USE_MATH_DEFINES in MCGIDI_headerSource

--- MCGIDI/Src/MCGIDI_headerSource.hpp
+++ MCGIDI/Src/MCGIDI_headerSource.hpp
@@ -10,6 +10,8 @@
 #ifndef MCGIDI_headerSource_hpp_included
 #define MCGIDI_headerSource_hpp_included 1
 
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include <climits>
 
 // From file: MCGIDI_URR.cpp
